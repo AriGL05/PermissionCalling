b6dfed0331e8c8eb4ffd070df5585f21998962a9
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-10-27 12:02:08 -0600

    changed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.basicpermisions"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.basicpermisions"
